a995cb6342dbafd2dc1cda906da141158214b0e1
Matias Castellanos <a2024112625@estudiantes.upsa.edu.bo>
2025-09-29 11:46:16 -0400

    correccion

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -5,7 +5,8 @@ namespace SeriesAPI.Data;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-     
+    public DbSet<Suit> Suits { get; set; }
+    public DbSet<Flash> Flashs { get; set; }
     public DbSet<Merlina> Merlinas { get; set; }
     public DbSet<ModernFamily> ModernFamilies { get; set; }
 }
