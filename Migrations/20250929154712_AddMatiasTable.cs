a3d82582b9393ef61c272be8ad06d4078e9ef0a4
Matias Castellanos <a2024112625@estudiantes.upsa.edu.bo>
2025-09-29 11:50:15 -0400

    Listooo

---
+++ Migrations/20250929154712_AddMatiasTable.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeriesAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMatiasTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Flashs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Personaje = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Actor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Edad = table.Column<int>(type: "int", nullable: false),
+                    EstadoCivil = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NetWorth = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flashs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Suits",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Personaje = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Actor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Edad = table.Column<int>(type: "int", nullable: false),
+                    EstadoCivil = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NetWorth = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suits", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Flashs");
+
+            migrationBuilder.DropTable(
+                name: "Suits");
+        }
+    }
+}
